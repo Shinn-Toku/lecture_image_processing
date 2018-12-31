80a481b3e3a94165d88abbf7400a7aa725ad39b8
Shinn-Toku <16ec081@ms.dendai.ac.jp>
2018-12-31 17:18:26 +0900

    change input image in kadai8.m

--- kadai8.m
+++ kadai8.m
@@ -6,11 +6,11 @@
 
 ORG = imread('airi_Base.png'); % 画像の読み込み
 ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
-imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+imagesc(ORG); colormap(gray); colorbar; axis image; % 画像の表示
 pause;
 IMG = ORG > 192; % 閾値192で二値化
-imagesc(IMG); colormap(gray); colorbar; % 画像の表示
+imagesc(IMG); colormap(gray); colorbar; axis image; % 画像の表示
 pause;
 IMG = bwlabeln(IMG);
-imagesc(IMG); colormap(jet); colorbar; % 画像の表示
+imagesc(IMG); colormap(jet); colorbar; axis image; % 画像の表示
 pause;
